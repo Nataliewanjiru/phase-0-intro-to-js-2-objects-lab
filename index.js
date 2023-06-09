b757d8529be784382e1f724073d8697c1c6efe6e
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-09 08:36:51 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var employee={name:"John",
+    streetAddress:"Nakuru"}
+
+
+    function updateEmployeeWithKeyAndValue(employee, key, value){
+     let newArray = {...employee}
+       newArray[key]=value;
+       return newArray;
+    }
+
+
+    let destructivelyUpdateEmployeeWithKeyAndValue =(employee, key,value) =>{
+        employee[key]=value;
+        return employee
+    }
+
+    let deleteFromEmployeeByKey = (employee,key)=>{
+        let newArray ={...employee};
+        delete newArray[key];
+        return newArray
+    }
+
+    let destructivelyDeleteFromEmployeeByKey = (employee, key) =>{
+        delete employee[key];
+        return employee;
+    }
+
+    
